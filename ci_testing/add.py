72ddba5cb98206ae3cb224aa9c383a6a4627571e
Taylor Barnes <tbarnes@Taylors-MacBook-Pro.local>
2017-09-28 10:40:54 -0400

    Initial commit

---
+++ ci_testing/add.py
@@ -0,0 +1,21 @@
+"""
+This is the add file.
+"""
+
+def add(x, y):
+    """
+    This is an add function
+
+    Parameters
+    ----------
+    x : {int, float, arb}
+        The first parametry to add
+    y : {int, float, arb}
+        The second parameter to add
+
+    Return
+    ------
+    ret : {int, float, arb}
+        The results of adding x and y
+    """
+    return x + y
