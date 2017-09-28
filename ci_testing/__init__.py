2a1fa1f62dbcdedaaf2b19563df96bf60a839837
Taylor Barnes <tbarnes@Taylors-MacBook-Pro.local>
2017-09-28 11:02:51 -0400

    Fixed bugs

--- ci_testing/__init__.py
+++ ci_testing/__init__.py
@@ -1,7 +1,5 @@
 """
+The is the base file for ci_testing
 """
 
-
-if __name__ == '__main__':
-    print(sys.argv)
-#    add(sys.argv[0],)
+from .add import *
--- tests/test_add.py
+++ tests/test_add.py
@@ -9,6 +9,6 @@ _add_tests = [(4, 3, 7),
               (5.3, 6.7, 12),
               ]
 
-@pytest.mark.parameterize("x,y,result", _add_test)
+@pytest.mark.parametrize("x,y,result", _add_tests)
 def test_add(x, y, result):
     assert result == pytest.approx(ct.add(x,y))
